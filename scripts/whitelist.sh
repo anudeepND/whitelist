70380d2d5c527b5547133170ecb2ef13f2305ccc
Anudeep <anudeep@protonmail.com>
2018-02-24 20:10:04 +0530

    Update whitelist.sh

--- scripts/whitelist.sh
+++ scripts/whitelist.sh
@@ -2,8 +2,9 @@
 # This script will download and add domains from the rep to whitelist.txt file.
 # Project homepage: https://github.com/anudeepND/whitelist
 # Licence: https://github.com/anudeepND/whitelist/blob/master/LICENSE
+# Created by Anudeep
 #================================================================================
-TICK="[ OK ${COL_NC}]"
+TICK="[ \e[1m\e[32m ^✔^t \e[0m]"
 echo -e " \e[1m This script will download and add domains from the repo to whitelist.txt \e[0m"
 sleep 1
 if [ $(dpkg-query -W -f='${Status}' gawk 2>/dev/null |  grep -c "ok installed") -eq 0 ];
