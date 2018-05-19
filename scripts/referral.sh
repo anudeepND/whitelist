3b3033105d80fe2d5d6402ff46aeded6670e5aac
Anudeep <anudeep@protonmail.com>
2018-05-19 17:29:58 +0530

    pihole -g updates only whitelist
    pihole -g --skip-download --whitelist-only 
    This command will update only the whitelist and doesn't update adlists.

--- scripts/referral.sh
+++ scripts/referral.sh
@@ -28,7 +28,7 @@ echo -e " ${TICK} \e[32m Removing duplicates... \e[0m"
 sudo gawk -i inplace '!a[$0]++' /etc/pihole/whitelist.txt
 wait
 echo -e " ${TICK} \e[32m Pi-hole gravity rebuilding lists... \e[0m"
-pihole -g > /dev/null
+pihole -g --skip-download --whitelist-only > /dev/null
 wait
 echo -e " ${TICK} \e[32m Done! \e[0m"
 
