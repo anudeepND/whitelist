c9bedb213d53d79316db477fe769cd6a71fe688f
Rob Gill <rrobgill@protonmail.com>
2018-05-19 15:55:09 +1000

    Make pi-hole reload lists after update
    This forces pihole to rebuild the list of blocked domains after updating the whitelist.

--- scripts/whitelist.sh
+++ scripts/whitelist.sh
@@ -27,8 +27,8 @@ sleep 0.5
 echo -e " ${TICK} \e[32m Removing duplicates... \e[0m"
 sudo gawk -i inplace '!a[$0]++' /etc/pihole/whitelist.txt
 wait
-echo -e " ${TICK} \e[32m Restarting DNS service \e[0m"
-pihole restartdns > /dev/null
+echo -e " ${TICK} \e[32m Pi-hole gravity rebuilding lists... \e[0m"
+pihole -g
 wait
 echo -e " ${TICK} \e[32m Done! \e[0m"
 
