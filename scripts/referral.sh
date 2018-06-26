dcd6320e543500cfbe2c4aa317bd967f32eb26ef
Anudeep <anudeep@protonmail.com>
2018-06-26 12:23:43 +0530

    Update referral.sh

--- scripts/referral.sh
+++ scripts/referral.sh
@@ -27,8 +27,8 @@ sleep 0.5
 echo -e " ${TICK} \e[32m Removing duplicates... \e[0m"
 sudo gawk -i inplace '!a[$0]++' /etc/pihole/whitelist.txt
 wait
-echo -e " ${TICK} \e[32m Pi-hole gravity rebuilding lists... \e[0m"
-pihole -g --skip-download --whitelist-only > /dev/null
+echo -e " ${TICK} \e[32m Pi-hole gravity rebuilding lists...This may take a while \e[0m"
+pihole -g > /dev/null
 wait
 echo -e " ${TICK} \e[32m Done! \e[0m"
 
