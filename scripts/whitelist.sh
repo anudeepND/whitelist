9b5923504e9a651a9ae1fadd31195cf3b5a10ffc
ChrisPhillips-cminion <chris.phillips@uk.ibm.com>
2018-12-24 15:10:55 +0000

    Update whitelist.sh
    Removed the need for gawk, removed unused waits and reduced some sleeps. 
    
    It also backs up the whitelist prior to the removal of duplications

--- scripts/whitelist.sh
+++ scripts/whitelist.sh
@@ -2,13 +2,13 @@
 # This script will download and add domains from the rep to whitelist.txt file.
 # Project homepage: https://github.com/anudeepND/whitelist
 # Licence: https://github.com/anudeepND/whitelist/blob/master/LICENSE
-# Created by Anudeep
+# Created by Anudeep (Slight change by cminion)
 #================================================================================
 TICK="[\e[32m ✔ \e[0m]"
 
 
 echo -e " \e[1m This script will download and add domains from the repo to whitelist.txt \e[0m"
-sleep 1
+sleep 0.1
 echo -e "\n"
 
 if [ "$(id -u)" != "0" ] ; then
@@ -16,28 +16,15 @@ if [ "$(id -u)" != "0" ] ; then
 	exit 2
 fi
 
-if ! (which gawk > /dev/null); then
-  echo -e " [...] \e[32m Installing gawk... \e[0m"
-  if (which apt-get > /dev/null); then
-       apt-get install gawk -qq > /dev/null
-  elif (which pacman > /dev/null); then
-       pacman -Sqy gawk > /dev/null
-  elif (which dnf > /dev/null); then
-       dnf install gawk > /dev/null
-  fi
-  wait
-  echo -e " ${TICK} \e[32m Finished \e[0m"
-fi
-
 curl -sS https://raw.githubusercontent.com/anudeepND/whitelist/master/domains/whitelist.txt | sudo tee -a /etc/pihole/whitelist.txt >/dev/null
 echo -e " ${TICK} \e[32m Adding to whitelist... \e[0m"
-sleep 0.5
+sleep 0.1
 echo -e " ${TICK} \e[32m Removing duplicates... \e[0m"
-sudo gawk -i inplace '!a[$0]++' /etc/pihole/whitelist.txt
-wait
+mv /etc/pihole/whitelist.txt /etc/pihole/whitelist.txt.old && cat /etc/pihole/whitelist.txt.old | sort | uniq >> /etc/pihole/whitelist.txt
+
 echo -e " [...] \e[32m Pi-hole gravity rebuilding lists. This may take a while... \e[0m"
 pihole -g > /dev/null
-wait
+ 
 echo -e " ${TICK} \e[32m Pi-hole's gravity updated \e[0m"
 echo -e " ${TICK} \e[32m Done! \e[0m"
 
