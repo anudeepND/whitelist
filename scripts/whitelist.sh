1fcf70f0934e7283002d0701e674319102939738
Anudeep <anudeep@protonmail.com>
2019-02-03 22:45:58 +0530

    Update whitelist.sh

--- scripts/whitelist.sh
+++ scripts/whitelist.sh
@@ -5,7 +5,12 @@
 # Created by Anudeep (Slight change by cminion)
 #================================================================================
 TICK="[\e[32m ✔ \e[0m]"
+PIHOLE_LOCATION="/etc/pihole"
 
+if [ -r config.cfg ]; then
+  echo "Reading user config...." >&2
+  source config.cfg
+fi
 
 echo -e " \e[1m This script will download and add domains from the repo to whitelist.txt \e[0m"
 sleep 0.1
@@ -16,14 +21,14 @@ if [ "$(id -u)" != "0" ] ; then
 	exit 2
 fi
 
-curl -sS https://raw.githubusercontent.com/anudeepND/whitelist/master/domains/whitelist.txt | sudo tee -a /etc/pihole/whitelist.txt >/dev/null
+curl -sS https://raw.githubusercontent.com/anudeepND/whitelist/master/domains/whitelist.txt | sudo tee -a "${PIHOLE_LOCATION}"/whitelist.txt >/dev/null
 echo -e " ${TICK} \e[32m Adding to whitelist... \e[0m"
 sleep 0.1
 echo -e " ${TICK} \e[32m Removing duplicates... \e[0m"
-mv /etc/pihole/whitelist.txt /etc/pihole/whitelist.txt.old && cat /etc/pihole/whitelist.txt.old | sort | uniq >> /etc/pihole/whitelist.txt
+mv "${PIHOLE_LOCATION}"/whitelist.txt "${PIHOLE_LOCATION}"/whitelist.txt.old && cat "${PIHOLE_LOCATION}"/whitelist.txt.old | sort | uniq >> "${PIHOLE_LOCATION}"/whitelist.txt
 
 echo -e " [...] \e[32m Pi-hole gravity rebuilding lists. This may take a while... \e[0m"
-pihole -g > /dev/null
+"${DOCKER_EXEC}" pihole -g > /dev/null
  
 echo -e " ${TICK} \e[32m Pi-hole's gravity updated \e[0m"
 echo -e " ${TICK} \e[32m Done! \e[0m"
