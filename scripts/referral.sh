4e4a9b92f0e5295494fd1c444d9050c4ab335998
Anudeep <anudeep@protonmail.com>
2019-02-03 22:49:01 +0530

    Update referral.sh

--- scripts/referral.sh
+++ scripts/referral.sh
@@ -5,7 +5,7 @@
 # Created by Anudeep
 #================================================================================
 TICK="[\e[32m ✔ \e[0m]"
-
+PIHOLE_LOCATION="/etc/pihole"
 
 echo -e " \e[1m This script will download and add domains from the repo to whitelist.txt \e[0m"
 sleep 1
@@ -17,16 +17,16 @@ if [ "$(id -u)" != "0" ] ; then
 fi
 
 
-curl -sS https://raw.githubusercontent.com/anudeepND/whitelist/master/domains/referral-sites.txt | sudo tee -a /etc/pihole/whitelist.txt >/dev/null
+curl -sS https://raw.githubusercontent.com/anudeepND/whitelist/master/domains/referral-sites.txt | sudo tee -a "${PIHOLE_LOCATION}"/whitelist.txt >/dev/null
 echo -e " ${TICK} \e[32m Adding to whitelist... \e[0m"
 sleep 0.5
 echo -e " ${TICK} \e[32m Removing duplicates... \e[0m"
 
-mv /etc/pihole/whitelist.txt /etc/pihole/whitelist.txt.old && cat /etc/pihole/whitelist.txt.old | sort | uniq >> /etc/pihole/whitelist.txt
+mv "${PIHOLE_LOCATION}"/whitelist.txt /etc/pihole/whitelist.txt.old && cat "${PIHOLE_LOCATION}"/whitelist.txt.old | sort | uniq >> "${PIHOLE_LOCATION}"/whitelist.txt
 
 wait
 echo -e " [...] \e[32m Pi-hole gravity rebuilding lists...This may take a while \e[0m"
-pihole -g > /dev/null
+${DOCKER_EXEC} pihole -g > /dev/null
 wait
 echo -e " ${TICK} \e[32m Pi-hole's gravity updated \e[0m"
 echo -e " ${TICK} \e[32m Done! \e[0m"
