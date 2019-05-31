08ed7f713be5ce4155e2ab16ccc41c719a0285ce
Anudeep <anudeep@protonmail.com>
2019-05-31 21:04:11 +0530

    Added support for Docker

--- scripts/whitelist.sh
+++ scripts/whitelist.sh
@@ -6,6 +6,7 @@
 #================================================================================
 TICK="[\e[32m ✔ \e[0m]"
 PIHOLE_LOCATION="/etc/pihole"
+GRAVITY_UPDATE_COMMAND="pihole -g"
 
 if [ -r config.cfg ]; then
   echo "Reading user config...." >&2
@@ -13,7 +14,8 @@ if [ -r config.cfg ]; then
 fi
 
 echo -e " \e[1m This script will download and add domains from the repo to whitelist.txt \e[0m"
-sleep 0.1
+echo -e " \e[1m All the domains in this list are safe to add and doesn't contain any tracking or adserving domains. \e[0m"
+sleep 1
 echo -e "\n"
 
 if [ "$(id -u)" != "0" ] ; then
@@ -22,13 +24,13 @@ if [ "$(id -u)" != "0" ] ; then
 fi
 
 curl -sS https://raw.githubusercontent.com/anudeepND/whitelist/master/domains/whitelist.txt | sudo tee -a "${PIHOLE_LOCATION}"/whitelist.txt >/dev/null
-echo -e " ${TICK} \e[32m Adding to whitelist... \e[0m"
+echo -e " ${TICK} \e[32m Adding domains to whitelist... \e[0m"
 sleep 0.1
 echo -e " ${TICK} \e[32m Removing duplicates... \e[0m"
 mv "${PIHOLE_LOCATION}"/whitelist.txt "${PIHOLE_LOCATION}"/whitelist.txt.old && cat "${PIHOLE_LOCATION}"/whitelist.txt.old | sort | uniq >> "${PIHOLE_LOCATION}"/whitelist.txt
 
 echo -e " [...] \e[32m Pi-hole gravity rebuilding lists. This may take a while... \e[0m"
-"${DOCKER_EXEC}" pihole -g > /dev/null
+${GRAVITY_UPDATE_COMMAND} /dev/null
  
 echo -e " ${TICK} \e[32m Pi-hole's gravity updated \e[0m"
 echo -e " ${TICK} \e[32m Done! \e[0m"
