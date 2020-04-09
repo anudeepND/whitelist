9ecfed52723da5a1fa74fda371277791e6822145
Anudeep <anudeep@protonmail.com>
2020-04-09 12:24:03 +0530

    Compatible with Pi-hole 5.0
    #125

--- scripts/whitelist.sh
+++ scripts/whitelist.sh
@@ -6,7 +6,7 @@
 #================================================================================
 TICK="[\e[32m ✔ \e[0m]"
 PIHOLE_LOCATION="/etc/pihole"
-GRAVITY_UPDATE_COMMAND="pihole -g"
+GRAVITY_UPDATE_COMMAND="pihole -w -q"
 
 echo -e " \e[1m This script will download and add domains from the repo to whitelist.txt \e[0m"
 echo -e "\n"
@@ -26,12 +26,12 @@ echo -e " ${TICK} \e[32m Removing duplicates... \e[0m"
 mv "${PIHOLE_LOCATION}"/whitelist.txt "${PIHOLE_LOCATION}"/whitelist.txt.old && cat "${PIHOLE_LOCATION}"/whitelist.txt.old | sort | uniq >> "${PIHOLE_LOCATION}"/whitelist.txt
 
 echo -e " [...] \e[32m Pi-hole gravity rebuilding lists. This may take a while... \e[0m"
-${GRAVITY_UPDATE_COMMAND} > /dev/null
+${GRAVITY_UPDATE_COMMAND} $(cat /etc/pihole/whitelist.txt | xargs) > /dev/null
  
 echo -e " ${TICK} \e[32m Pi-hole's gravity updated \e[0m"
 echo -e " ${TICK} \e[32m Done! \e[0m"
 
-
+echo -e " \e[1m  Buy me a coffee: https://paypal.me/anudeepND \e[0m"
 echo -e " \e[1m  Star me on GitHub, https://github.com/anudeepND/whitelist \e[0m"
 echo -e " \e[1m  Happy AdBlocking :)\e[0m"
 echo -e "\n\n"
