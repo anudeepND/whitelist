5b83737783a4102a4125293d99d78e51b082c3f7
mwoolweaver <michael@woolweaver.bid>
2020-09-02 10:47:08 -0500

    remove error prone total_changes function introduced in #158 also fix merge issue of #163

--- scripts/whitelist.py
+++ scripts/whitelist.py
@@ -166,19 +166,16 @@ if db_exists:
 
         sqliteConnection.commit()
 
-        # total_changes is returning 2x the actual value. ¯\_(ツ)_/¯
-        # if I made a mistake, please create a PR
-        numberOfDomains = sqliteConnection.total_changes
-        if numberOfDomains > 1:
-            numberOfDomains = numberOfDomains // 2
-        # print(f'[i] {numberOfDomains} domains are added to whitelist out of {len(whitelist_remote)}')
-        print("[i] {} domains are added to whitelist out of {}" .format(
-            numberOfDomains, len(whitelist_remote)))
-        total_domains = cursor.execute(
-            " SELECT * FROM domainlist WHERE type = 0 OR type = 2 ")
-        # print(f'[i] There are a total of {len(total_domains.fetchall())} domains in your whitelist')
-        print("[i] There are a total of {} domains in your whitelist" .format(
-            len(total_domains.fetchall())))
+        # find only the domains we added
+        number_domains = cursor.execute(" SELECT * FROM domainlist WHERE type = 0 AND comment LIKE '%qjz9zk%' ")
+        
+        numberDomains = len(number_domains.fetchall())
+        
+        #print(f'[i] {numberOfDomains} domains are added to whitelist out of {len(whitelist_remote)}')
+        print("[i] {} domains are added to whitelist out of {}" .format(numberDomains, len(whitelist_remote)))
+        total_domains = cursor.execute(" SELECT * FROM domainlist WHERE type = 0 OR type = 2 ") # only show exact whitelisted domains as we don't add/remove regex
+        #print(f'[i] There are a total of {len(total_domains.fetchall())} domains in your whitelist')
+        print("[i] There are a total of {} domains in your whitelist" .format(len(total_domains.fetchall())))
         cursor.close()
 
     except sqlite3.Error as error:
